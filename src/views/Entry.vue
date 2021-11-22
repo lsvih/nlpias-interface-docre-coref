801568fbebd2951872acfef560d621f19c6ed081
lsvih <lsvih@qq.com>
2021-11-22 19:45:51 +0800

    Add feature: Display pre-annotated merged vertexset.

--- src/views/Entry.vue
+++ src/views/Entry.vue
@@ -257,7 +257,10 @@ export default {
                 let text, label, predict
                 [text, label, predict] = data
                 this.text = JSON.parse(text)
-                this.merged_vertex = []
+                if ('merged_vertex' in this.text)
+                    this.merged_vertex = this.text['merged_vertex']
+                else
+                    this.merged_vertex = []
                 this.predict = predict || []
                 if (label) {
                     this.text = JSON.parse(label)
